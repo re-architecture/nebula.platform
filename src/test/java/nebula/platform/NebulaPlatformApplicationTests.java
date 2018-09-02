5f83e52b3cdcff959400ff810a1f87251034a305
sunxiaofeng <sunxiaofeng@msn.com>
2018-09-02 11:23:09 +0800

    Initial commit

---
+++ src/test/java/nebula/platform/NebulaPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package nebula.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NebulaPlatformApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
